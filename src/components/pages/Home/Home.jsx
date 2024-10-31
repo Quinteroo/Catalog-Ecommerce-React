72c14f0b13f008f377d1977303c0ed76b732e5ca
qugodev <146204443+Quinteroo@users.noreply.github.com>
2024-10-31 15:55:44 +0100

    Update Home con Footer

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -2,11 +2,16 @@ import "./Home.css"
 import Header from "../../layout/Header/Header.jsx"
 import Navbar from "../../Navbar/Navbar.jsx"
 import Logo from "../../Logos/Logo/Logo.jsx"
+import Footer from "../../layout/Footer/Footer.jsx"
 
 const Home = () => {
   return (
     <>
       <Header logo={<Logo />} navbar={<Navbar />} />
+      <main>
+
+      </main>
+      <Footer github={"github"} linkedin={"linkedin"} />
 
     </>
   )
