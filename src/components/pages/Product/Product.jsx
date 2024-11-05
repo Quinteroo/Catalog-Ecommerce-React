a1eb610233783ad1a9d7aa42b540eb3d24ac60e1
quintero git <hello.quinteroo@gmail.com>
2024-11-06 00:25:45 +0100

    Product.jsx

--- src/components/pages/Product/Product.jsx
+++ src/components/pages/Product/Product.jsx
@@ -1,8 +1,21 @@
 import "./Product.css"
+import Header from "../../layout/Header/Header.jsx"
+import Navbar from "../../Navbar/Navbar.jsx"
+import LogoLinkHome from "../../Logos/LogoLinkHome/LogoLinkHome.jsx"
+import Footer from "../../layout/Footer/Footer.jsx"
+import GithubButton from "../../Buttons/GithubButton/GithubButton.jsx"
+import LinkedInButton from "../../Buttons/LinkedInButton/LinkedInButton"
+import { Link } from "react-router-dom"
 
 const Product = () => {
   return (
-    <div>Product</div>
+    <>
+      <Header logo={<LogoLinkHome />} navbar={<Navbar />} />
+      <main>
+
+      </main>
+      <Footer github={<GithubButton />} linkedin={<LinkedInButton />} />
+    </>
   )
 }
 
