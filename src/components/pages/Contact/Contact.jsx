a903b977cd2d0f07445721b2f486cf63107258cd
qugodev <146204443+Quinteroo@users.noreply.github.com>
2024-11-06 18:28:44 +0100

    Update Contact.jsx

--- src/components/pages/Contact/Contact.jsx
+++ src/components/pages/Contact/Contact.jsx
@@ -5,14 +5,19 @@ import Navbar from "../../Navbar/Navbar.jsx"
 import Footer from "../../layout/Footer/Footer.jsx"
 import GithubButton from "../../Buttons/GithubButton/GithubButton.jsx"
 import LinkedInButton from "../../Buttons/LinkedInButton/LinkedInButton.jsx"
+import Form from "../../Form/Form.jsx"
 
 const Contact = () => {
   return (
     <>
       <Header logo={<LogoLinkHome />} navbar={<Navbar />} />
       <main>
-
-
+        <section>
+          <div className="div-img-form">
+            <img src="/assets/img-form.png" alt="imagen sección de contacto" title="imagen de sección de contacto" loading="lazy" />
+          </div>
+          <Form />
+        </section>
       </main>
       <Footer github={<GithubButton />} linkedin={<LinkedInButton />} />
     </>
