fcef03b5c2b7d18935580097e609eff0c21b72af
qugodev <146204443+Quinteroo@users.noreply.github.com>
2024-11-06 20:51:27 +0100

    Add files via upload

---
+++ src/components/Form/InputField/InputField.jsx
@@ -0,0 +1,20 @@
+import "./InputField.css"
+import React from "react"
+
+
+
+const InputField = React.memo(({ label, name, value, onChange }) => (
+  <div>
+    <label htmlFor={name}>{label}:</label>
+    <input
+      type="text"
+      id={name}
+      name={name}
+      value={value}
+      onChange={onChange}
+    />
+  </div>
+));
+
+
+export default InputField
