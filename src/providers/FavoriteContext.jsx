60b2e7753f853b26c763b2d5fffd246e63e87610
qugodev <146204443+Quinteroo@users.noreply.github.com>
2024-11-07 15:06:33 +0100

    Update FavoriteContext.jsx

--- src/providers/FavoriteContext.jsx
+++ src/providers/FavoriteContext.jsx
@@ -9,10 +9,8 @@ const FavoriteProvider = ({ children }) => {
 
   const toggleFavorite = (product) => {
     if (favs.includes(product)) {
-      // Elimina el producto de favoritos si ya está en la lista
       setFavs(favs.filter((fav) => fav !== product));
     } else {
-      // Agrega el producto a favoritos si no está en la lista
       setFavs([...favs, product]);
     }
   };
