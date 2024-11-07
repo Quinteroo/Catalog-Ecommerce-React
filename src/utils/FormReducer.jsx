2b18b3575535e2ba5b42988a2452af5cf8ae2bf6
qugodev <146204443+Quinteroo@users.noreply.github.com>
2024-11-07 15:00:53 +0100

    Update FormReducer.jsx

--- src/utils/FormReducer.jsx
+++ src/utils/FormReducer.jsx
@@ -1,23 +1,18 @@
 import React, { useReducer, useCallback } from 'react';
 
-// Estado inicial
+
 const INITIAL_FORM_STATE = {
   nombre: '',
   email: '',
   mensaje: ''
 };
 
-// Reducer para actualizar el estado
+
 function formReducer(state, action) {
   switch (action.type) {
     case 'UPDATE_FIELD':
-      return { ...state, [action.field]: action.value };
+      return { ...state, [action.payload.field]: action.payload.value };
     default:
       return state;
   }
 }
-
-
-
-
-
